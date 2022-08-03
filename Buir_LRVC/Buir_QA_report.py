437ae48edce61f4d33fbb45442a2e8a17cd7400c
SubusLane <100224598+SubusLane@users.noreply.github.com>
2022-08-03 13:19:13 +0530

    Update Buir_QA_report.py

--- Buir_LRVC/Buir_QA_report.py
+++ Buir_LRVC/Buir_QA_report.py
@@ -24,7 +24,7 @@ def get_QAreport(job_id,targetdir):
     targetdir = targetdir
     
     
-    headers = {'Authorization': 'Token token=pvgbMxCLGsS72ZwsnuYj'}
+    headers = {'Authorization': 'Token token=###'}
     data = {'type': 'full'}
     response = requests.post('https://api.appen.com/v1/jobs/'+job_id+'/regenerate', headers=headers, data=data)    
     print(response) #Just for debug
